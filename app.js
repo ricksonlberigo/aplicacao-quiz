d39c876dd113ecfb04870e4a4fee9a0baa4c0e68
ricksonl.berigo <ricksonl.berigo@outlook.com>
2023-09-06 08:51:32 -0300

    finishing the application.

--- app.js
+++ app.js
@@ -1,1 +1,57 @@
-console.log('Hello word')
+const form = document.querySelector('.quiz-form')
+const finalScoreContainer = document.querySelector('.final-score-container')
+const correctAnswers = ['D', 'A', 'C', 'A', 'A']
+
+let score = 0
+
+const getUserAnswers = () => {
+  let userAnswers = []
+
+  correctAnswers.forEach((_, index) => {
+    const userAnswer = form[`inputQuestion${index + 1}`].value
+    userAnswers.push(userAnswer)
+  })
+
+  return userAnswers
+}
+
+const calculateUserScore = userAnswers => {
+  userAnswers.forEach((userAnswer, index) => {
+    const isUserAnswerCorrect = userAnswer === correctAnswers[index]
+    if (isUserAnswerCorrect) {
+      score += 20
+    }
+  })
+}
+
+const showFinalScore = () => {
+  scrollTo({
+    top: 0,
+    left: 0,
+    behavior: 'smooth',
+  })
+  finalScoreContainer.classList.remove('d-none')
+}
+
+const animatedFinalScore = () => {
+  let counter = 0
+
+  const timer = setInterval(() => {
+    if (counter === score) {
+      clearInterval(timer)
+    }
+
+    finalScoreContainer.querySelector('span').textContent = `${counter++}%`
+  }, 15)
+}
+
+form.addEventListener('submit', event => {
+  event.preventDefault()
+  score = 0
+
+  const userAnswers = getUserAnswers()
+
+  calculateUserScore(userAnswers)
+  showFinalScore()
+  animatedFinalScore()
+})
